3433c97a05902d31e3089b019578389417bad604
walkerrout04@gmail.com <walkerrout04@gmail.com>
2024-06-12 15:27:44 -0700

    Update with older

--- pdf_bot.py
+++ pdf_bot.py
@@ -7,11 +7,16 @@ from langchain.callbacks.base import BaseCallbackHandler
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Neo4jVector
 from streamlit.logger import get_logger
-
 from chains import (
-  load_embedding_model,
-  load_llm,
+    load_embedding_model,
+    load_llm,
 )
+
+# load api key lib
+from dotenv import load_dotenv
+
+load_dotenv(".env")
+
 
 url = os.getenv("NEO4J_URI")
 username = os.getenv("NEO4J_USERNAME")
@@ -24,61 +29,67 @@ os.environ["NEO4J_URL"] = url
 
 logger = get_logger(__name__)
 
+
 embeddings, dimension = load_embedding_model(
-  embedding_model_name, config={"ollama_base_url": ollama_base_url}, logger=logger
+    embedding_model_name, config={"ollama_base_url": ollama_base_url}, logger=logger
 )
 
-class StreamHandler(BaseCallbackHandler):
-  def __init__(self, container, initial_text=""):
-    self.container = container
-    self.text = initial_text
 
-  def on_llm_new_token(self, token: str, **kwargs) -> None:
-    self.text += token
-    self.container.markdown(self.text)
+class StreamHandler(BaseCallbackHandler):
+    def __init__(self, container, initial_text=""):
+        self.container = container
+        self.text = initial_text
+
+    def on_llm_new_token(self, token: str, **kwargs) -> None:
+        self.text += token
+        self.container.markdown(self.text)
+
 
 llm = load_llm(llm_name, logger=logger, config={"ollama_base_url": ollama_base_url})
 
+
 def main():
-  st.header("📄Chat with your pdf file")
+    st.header("📄Chat with your pdf file")
 
-  # upload a your pdf file
-  pdf = st.file_uploader("Upload your PDF", type="pdf")
+    # upload a your pdf file
+    pdf = st.file_uploader("Upload your PDF", type="pdf")
 
-  if pdf is not None:
-    pdf_reader = PdfReader(pdf)
+    if pdf is not None:
+        pdf_reader = PdfReader(pdf)
 
-    text = ""
-    for page in pdf_reader.pages:
-      text += page.extract_text()
+        text = ""
+        for page in pdf_reader.pages:
+            text += page.extract_text()
 
-    # langchain_textspliter
-    text_splitter = RecursiveCharacterTextSplitter(
-      chunk_size=1000, chunk_overlap=200, length_function=len
-    )
+        # langchain_textspliter
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=1000, chunk_overlap=200, length_function=len
+        )
 
-    chunks = text_splitter.split_text(text=text)
+        chunks = text_splitter.split_text(text=text)
 
-    # Store the chunks part in db (vector)
-    vectorstore = Neo4jVector.from_texts(
-      chunks,
-      url=url,
-      username=username,
-      password=password,
-      embedding=embeddings,
-      index_name="pdf_bot",
-      node_label="PdfBotChunk",
-      pre_delete_collection=True,  # Delete existing PDF data
-    )
-    qa = RetrievalQA.from_chain_type(
-      llm=llm, chain_type="stuff", retriever=vectorstore.as_retriever()
-    )
+        # Store the chunks part in db (vector)
+        vectorstore = Neo4jVector.from_texts(
+            chunks,
+            url=url,
+            username=username,
+            password=password,
+            embedding=embeddings,
+            index_name="pdf_bot",
+            node_label="PdfBotChunk",
+            pre_delete_collection=True,  # Delete existing PDF data
+        )
+        qa = RetrievalQA.from_chain_type(
+            llm=llm, chain_type="stuff", retriever=vectorstore.as_retriever()
+        )
 
-    query = st.text_input("Ask questions about your PDF file")
+        # Accept user questions/query
+        query = st.text_input("Ask questions about your PDF file")
 
-    if query:
-      stream_handler = StreamHandler(st.empty())
-      qa.invoke(query, callbacks=[stream_handler])
+        if query:
+            stream_handler = StreamHandler(st.empty())
+            qa.run(query, callbacks=[stream_handler])
+
 
 if __name__ == "__main__":
-  main()
+    main()
